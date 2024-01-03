83967af8fb292f36bcf621585d000185e522d370
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-03 16:46:05 +0100

    added logo to header

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,10 +1,19 @@
 'use client'
 
 import React from 'react'
+import Image from 'next/image'
+import logo from '../../../public/images/logo.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <nav>
+            <div className='logo'>
+                <Image src={logo} alt='logo' width={40} />
+                <h2>Logo</h2>
+            </div>
+        </nav>
+    </div>
   )
 }
 
