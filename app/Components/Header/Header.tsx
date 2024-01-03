6eb0bca85a7dfc0f7d8bbf7260686558802389e5
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-03 18:08:13 +0100

    added text and btns

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -5,7 +5,14 @@ import Image from 'next/image';
 import logo from '../../../public/images/logo.png';
 import Button from '../Button/Button';
 import styled from 'styled-components';
-import { FaRegHandPointer } from "react-icons/fa";
+import { FaRegHandPointer } from 'react-icons/fa';
+import { MdOutlineRocketLaunch } from "react-icons/md";
+import { Abril_Fatface } from 'next/font/google';
+
+const abril = Abril_Fatface({
+	subsets: ['latin'],
+	weight: ['400'],
+});
 
 function Header() {
 	return (
@@ -31,9 +38,28 @@ function Header() {
 					<li>
 						<a href='#'>About</a>
 					</li>
-					<Button name='Push button' icon={<FaRegHandPointer/>}/>
+					<Button name='Push button' icon={<FaRegHandPointer />} />
 				</ul>
 			</nav>
+			<div className='header-content'>
+				<div>
+					<h1 className={abril.className}>Framer Motion Test</h1>
+					<p>
+						Production-ready declarative animations. A simple declarative syntax
+						means you write less code. Less code means your codebase is easier
+						to read and maintain.
+					</p>
+                    <div className='buttons'>
+                    <Button name='Get Started' 
+                    background='#f2994a'
+                    color='#fff'
+                    border='1px solid #f2994a'
+                    icon={<MdOutlineRocketLaunch />}
+                    /> 
+                    <Button name='Read More'/>
+                    </div>
+				</div>
+			</div>
 		</HeaderStyled>
 	);
 }
