9e7f3c762956e91fb9c1c4e9588fb69e93a5a949
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-04 23:12:04 +0100

    added cards array

---
+++ app/utils/cards.js
@@ -0,0 +1,43 @@
+import channel from '/public/images/channel.jpeg'
+import channel2 from '/public/images/channel2.jpeg'
+import denise from 'public/images/denise.jpeg'
+import circles from 'public/images/9781304.jpeg'
+import fluid from 'public/images/fluid.jpeg'
+
+export const cards = [
+  {
+    id: 1,
+    title: "Channel Cube",
+    description: "Abstract Water Cube",
+    image: channel,
+    link: "/channel",
+  },
+  {
+    id: 2,
+    title: "Fire Cube",
+    description: "Cube full of fire",
+    image: channel2,
+    link: "/channel2",
+  },
+  {
+    id: 3,
+    title: "Tickets",
+    description: "Magical Tickets for Fire Freezing",
+    image: denise,
+    link: "/denise",
+  },
+  {
+    id: 4,
+    title: "Circle",
+    description: "Circles a full of pink and purple colors",
+    image: circles,
+    link: "/9781304",
+  },
+  {
+    id: 5,
+    title: "Fluid",
+    description: "A insane yellow fluid",
+    image: fluid,
+    link: "/fluid",
+  },
+];
