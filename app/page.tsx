5895d30f8b132cbf5cd212ce3d74893a6be8ff4f
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-04 22:28:25 +0100

    created section layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import styled from 'styled-components';
 import Header from './Components/Header/Header';
+import SectionLayout from './Components/SectionLayout';
 
 export default function Home() {
 	return (
 		<>
 			<Header />
+			<MainStyled>
+				<SectionLayout>
+					
+				</SectionLayout>
+			</MainStyled>
 		</>
 	);
 }
+
+const MainStyled = styled.main`
+min-height: 100vh;
+width: 100%;
+`;
