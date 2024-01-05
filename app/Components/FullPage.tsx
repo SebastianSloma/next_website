41e4d3d390e4c7deb0dd8d2d0b552584e96780a3
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-05 18:38:35 +0100

    styled full page tsx

--- app/Components/FullPage.tsx
+++ app/Components/FullPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import styled from 'styled-components'
 
 const FullPage = () => {
   return (
@@ -18,5 +19,27 @@ const FullPage = () => {
     </div>
   )
 }
+
+const FullPageStyled = styled.div`
+.image {
+  width: calc(100% - 10rem);
+  height: 1000px;
+
+  position: relative;
+  border-radius: 1rem;
+  border-radius: 8px;
+
+  border: 1px solid var(--color-border);
+  transition: border 0.3s ease-in-out;
+
+  img {
+    padding: 1.5rem;
+  }
+
+  &:hover {
+    border: 1px solid #f2994a;
+  }
+}
+`
 
 export default FullPage
