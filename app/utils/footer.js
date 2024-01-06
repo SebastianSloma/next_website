0179ab86bf8b9ca6c0f0d958d4194e86e7072c0f
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-06 07:40:51 +0100

    added footer array

---
+++ app/utils/footer.js
@@ -0,0 +1,75 @@
+const footer = [
+    {
+      title: "My Account",
+      links: [
+        {
+          name: "Profile",
+          link: "/profile",
+        },
+        {
+          name: "Settings",
+          link: "/settings",
+        },
+        {
+          name: "Logout",
+          link: "/logout",
+        },
+        {
+          name: "Help",
+          link: "/help",
+        },
+      ],
+    },
+    {
+      title: "Resources",
+      links: [
+        {
+          name: "Help Center",
+          link: "/help",
+        },
+        {
+          name: "Getting started",
+          link: "/started",
+        },
+        {
+          name: "Introduction",
+          link: "/introduction",
+        },
+        {
+          name: "Overview",
+          link: "/overview",
+        },
+      ],
+    },
+    {
+      title: "Useful Links",
+      links: [
+        {
+          name: "React",
+          link: "/react",
+        },
+        {
+          name: "Tailwind",
+          link: "/tailwind",
+        },
+        {
+          name: "Create",
+          link: "/create",
+        },
+        {
+          name: "Next.js",
+          link: "/nextjs",
+        },
+        {
+          name: "Framer Motion",
+          link: "/framermotion",
+        },
+        {
+          name: "Privacy & Policy",
+          link: "/privacy",
+        },
+      ],
+    },
+  ];
+  
+  export default footer;
