f8b5aca74b0263f76f146dc70554de1c7565b6d8
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-06 07:50:14 +0100

    create footer

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -4,6 +4,13 @@ import Image from 'next/image'
 import React from 'react'
 import styled from 'styled-components'
 import logo from '/public/images/logo.png'
+import footer from '../utils/footer'
+import { VscGithub } from "react-icons/vsc";
+import { FaSquareXTwitter,FaSquareInstagram, FaDiscord  } from "react-icons/fa6";
+import { GrLinkedin } from "react-icons/gr";
+
+
+
 
 const Footer = () => {
   return (
@@ -13,6 +20,10 @@ const Footer = () => {
                 <div className="logo">
                     <Image src={logo} alt='logo' width={36}/>
                     <h2>Logo</h2>
+                </div>
+                <div className="input">
+                    <input type="text" placeholder='Subscribe here...' />
+                    <button>&#8594</button>
                 </div>
             </div>
         </nav>
