ddfbd3f0b8d85aa30bfdf9fbafa099e61eba06f8
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-10 12:28:51 +0100

    added framer motion to full page section

--- app/Components/FullPage.tsx
+++ app/Components/FullPage.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import Image from 'next/image'
 import styled from 'styled-components'
+import { motion, useScroll, useTransform} from 'framer-motion'
 
 const FullPage = () => {
+  const secRef = React.useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress} = useScroll({
+    target:secRef,
+    offset:['start end', 'end start']
+  })
+
+  const scale = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
+  const xTransform = useTransform(
+    scrollYProgress,
+    [1, 0.5, 0.1, 0],
+    [-1000, 0, 0, 0]
+  );
+
   return (
-    <FullPageStyled>
-        <div className="image">
+    <FullPageStyled ref={secRef}>
+        <motion.div className="image" style={{
+          scale: scale,
+          x: xTransform,
+        }}>
             <Image 
             src='/images/andrea.jpeg'
             alt='quote'
@@ -15,7 +33,7 @@ const FullPage = () => {
                 objectPosition:'center'
             }}
             />
-        </div>
+        </motion.div>
     </FullPageStyled>
   )
 }
